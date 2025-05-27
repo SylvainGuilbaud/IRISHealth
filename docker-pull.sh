15e04e89b467005e490ece6102e4d782bc571208
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2025-05-27 20:59:09 +0200

    merge

---
+++ docker-pull.sh
@@ -0,0 +1,4 @@
+docker pull containers.intersystems.com/intersystems/irishealth-community:latest-em
+docker pull containers.intersystems.com/intersystems/irishealth-community:latest-cd
+docker pull containers.intersystems.com/intersystems/irishealth-community:latest-preview
+docker pull containers.intersystems.com/intersystems/webgateway:latest-preview
